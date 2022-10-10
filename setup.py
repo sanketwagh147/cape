7f6f8519b648da5d6998b7836ffb87cbe5a4b34d
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2022-10-10 17:52:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re
 
 import setuptools
 
-with open(os.path.join(os.path.dirname(__file__), "ffn", "__init__.py"), "r") as fp:
+with open(os.path.join(os.path.dirname(__file__), "ffn", "cape/__init__.py"), "r") as fp:
     version = re.search(
         "^__version__ = \\((\\d+), (\\d+), (\\d+)\\)$", fp.read(), re.MULTILINE
     ).groups()
