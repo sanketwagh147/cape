aca68f6c0a955324866452d298e17c9a28128fad
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2022-10-10 17:50:09 -0500

    Upload setup.py

---
+++ cape/setup.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), "ffn", "__init__.py"), "r") as fp:
+    version = re.search(
+        "^__version__ = \\((\\d+), (\\d+), (\\d+)\\)$", fp.read(), re.MULTILINE
+    ).groups()
+
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst"), "r") as fp:
+    description = fp.read().replace("\r\n", "\n")
+
+setuptools.setup(
+    name="cape",
+    version=".".join(version),
+    author="Nathan Ramos, CFA©",
+    author_email="info@nrcapitalmanagement.com",
+    description="Financial application for Python",
+    keywords="python finance quant valuation",
+    url="https://github.com/nathanramoscfa/cape",
+    license="MIT",
+    packages=["cape", "cape/cape"],
+    long_description=description,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python",
+    ],
+)
