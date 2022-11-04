ebcaf3234ba39529f9584a9e45fbb804f77d51e4
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2022-11-04 17:02:57 -0500

    Update cape.py

--- cape/cape.py
+++ cape/cape.py
@@ -80,16 +80,16 @@ def chart(etf_ticker, chart_num=1):
     if chart_num == 1:
         display(Image(
             'https://raw.githubusercontent.com/nathanramoscfa/cape/main/django_apps/mysite/forecast/static/forecast'
-            '/images/sample_regression_{}.png'.format(index_ticker)))
+            '/images/sample_regression_{}.jpg'.format(index_ticker)))
     elif chart_num == 2:
         display(Image(
             'https://raw.githubusercontent.com/nathanramoscfa/cape/main/django_apps/mysite/forecast/static/forecast'
-            '/images/sample_observed_forecast_{}.png'.format(index_ticker)))
+            '/images/sample_observed_forecast_{}.jpg'.format(index_ticker)))
     elif chart_num == 3:
         display(Image('https://raw.githubusercontent.com/nathanramoscfa/cape/main/django_apps/mysite/forecast/static'
-                      '/forecast/images/long_term_pe_ratio_{}.png'.format(index_ticker)))
+                      '/forecast/images/long_term_pe_ratio_{}.jpg'.format(index_ticker)))
     elif chart_num == 4:
         display(Image('https://raw.githubusercontent.com/nathanramoscfa/cape/main/django_apps/mysite/forecast/static'
-                      '/forecast/images/expected_fwd_return_5y_{}.png'.format(index_ticker)))
+                      '/forecast/images/expected_fwd_return_5y_{}.jpg'.format(index_ticker)))
     else:
         raise ValueError('Invalid chart_num parameter. Must input integer from 1 to 5 corresponding to desired chart.')
