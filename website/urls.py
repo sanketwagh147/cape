86efd2341400866ee0b66902be49d3f10c030055
DiablosWhisper <shelyakingleb@gmail.com>
2023-08-23 18:55:27 +0300

    update: default login + logout, stripe integration, email verification

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forecast/', include('forecast.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('allauth.urls')),
 ]
